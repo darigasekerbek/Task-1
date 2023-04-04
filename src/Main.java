ce42d42c15a0e50520c20ac00136da2eb03288ee
Abylai <221714@astanait.edu.kz>
2023-04-04 09:28:20 +0200

    Implementation of findMin of arr Implementation of findAverage of arr

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] array = new int[n];
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            array[i] = sc.nextInt();
+            arr[i] = sc.nextInt();
         }
-        int min = findMin(array, n);
-        System.out.println("Min is " + min);
+        int min = findMin(arr, n);
+        System.out.println("Minimum is " + min);
+        double aver = findAverage(arr, n);
+        System.out.println("Average is " + aver);
 
     }
 
     public static int findMin(int[] arr, int n) {
-        if (n == 1)
-            return arr[0];
+        if (n == 1) {
+            return arr[0];}
         else {
             int min = findMin(arr, n - 1);
             return Math.min(min, arr[n - 1]);
         }
     }
-}
+    public static double findAverage(int[] arr, int n) {
+        if (n==0) {
+            return 0;
+        }
+            double sum = findAverage(arr,n-1)*(n-1)+arr[n-1];
+            return sum/n;
+        }
+    }
 
